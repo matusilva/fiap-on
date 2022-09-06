f89c7e6704a93f9084ed518529f0e6b02ecaf73d
matusilva <viniciusmatheus.dev@gmail.com>
2022-09-05 23:30:19 -0300

    decisões encadeadas

--- python/OT/Decisoes/Decisao_IF.py
+++ python/OT/Decisoes/Decisao_IF.py
@@ -1,7 +1,10 @@
 nome = input("Digite o nome: ")
 idade = int(input("Digite a idade: "))
+doenca_contagiosa = input("Suspeita de doença contagiosa? ").upper()
 
 if idade >= 65:
     print("O paciente " + nome + " POSSUI atendimento prioritário!")
+elif doenca_contagiosa == "SIM":
+    print("O paciente " + nome + " deve ser direcionado para sala de espera reservada.")
 else:
     print("O paciente " + nome + " NÃO possui atendimento prioritário!")
