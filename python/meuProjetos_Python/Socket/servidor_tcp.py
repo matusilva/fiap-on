a04f4dd84a70abc90c4e940cb4d4aad83f9574f0
matusilva <viniciusmatheus.dev@gmail.com>
2022-09-10 01:13:06 -0300

    corrigindo o envio da informacao de conexao como tupla

--- python/meuProjetos_Python/Socket/servidor_tcp.py
+++ python/meuProjetos_Python/Socket/servidor_tcp.py
@@ -4,7 +4,7 @@ server = "127.0.0.1"
 port = 1010
 
 obj_socket = socket(AF_INET, SOCK_STREAM)
-obj_socket.bind(server, port)
+obj_socket.bind((server, port))
 obj_socket.listen(2)
 
 print("Aguardando cliente...")
